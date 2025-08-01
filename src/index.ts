03044d8ba2c771900e857b63022514392b76c2a5
vinothvino42 <vinothvino42@gmail.com>
2025-08-02 00:05:34 +0530

    feat: add mcp server to handle job search

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,119 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import {
+  CallToolRequestSchema,
+  ListToolsRequestSchema,
+} from "@modelcontextprotocol/sdk/types.js";
+import { fetchJobs } from "./job-scraper";
+import { Job } from "./types/job";
+import { z } from "zod";
 
-const server = new McpServer(
+const server = new Server(
   {
     name: "Technopark Job Search",
     version: "1.0.0",
   },
   {
     capabilities: {
-      tools: {},
+      tools: {
+        callSchema: CallToolRequestSchema,
+      },
     },
   }
 );
 
-const transport = new StdioServerTransport();
-server.connect(transport).catch((error) => {
-  console.error("Server connection error:", error);
-  process.exit(1);
+const TECHNOPARK_BASE_URL = "https://technopark.in";
+
+const JobSearchSchema = z.object({
+  role: z.string({
+    required_error: "Role is required",
+    description: "Search query for job titles",
+  }),
 });
+
+type JobSearchInput = z.infer<typeof JobSearchSchema>;
+
+async function jobSearch(
+  endpoint: string,
+  params: Record<string, string> = {}
+): Promise<Job[]> {
+  const url = new URL(`${TECHNOPARK_BASE_URL}${endpoint}`);
+  for (const [key, value] of Object.entries(params)) {
+    url.searchParams.append(key, value);
+  }
+  console.log(`URL : ${url.toString()}`);
+
+  try {
+    const jobs: Job[] = await fetchJobs(url.toString());
+    return Promise.resolve(jobs);
+  } catch (error) {
+    return Promise.reject();
+  }
+}
+
+server.setRequestHandler(ListToolsRequestSchema, async () => {
+  return {
+    tools: [
+      {
+        name: "technopark-job-search",
+        description: "Search for jobs by title or keyword",
+        inputSchema: {
+          type: "object",
+          properties: {
+            role: {
+              type: "string",
+              description: "Search role",
+            },
+          },
+          required: ["role"],
+        },
+      },
+    ],
+  };
+});
+
+server.setRequestHandler(CallToolRequestSchema, async (request, _extra) => {
+  try {
+    switch (request.params.name) {
+      case "technopark-job-search": {
+        const role = request.params.arguments?.role as string;
+        const jobs: Job[] = await jobSearch("/job-search", {
+          search: role,
+        });
+        const jobsData = JSON.stringify(jobs, null, 2);
+        return {
+          content: [
+            {
+              type: "text",
+              text: jobsData,
+              mimeType: "application/json",
+            },
+          ],
+        };
+      }
+      default:
+        throw new Error("Tool not found");
+    }
+  } catch (error) {
+    return {
+      content: [
+        {
+          type: "text",
+          text: `Error: ${
+            error instanceof Error ? error.message : "Something went wrong"
+          }`,
+        },
+      ],
+    };
+  }
+});
+
+async function main() {
+  const transport = new StdioServerTransport();
+  server.connect(transport).catch((error) => {
+    console.error("Server connection error:", error);
+    process.exit(1);
+  });
+}
+
+main();
---
+++ src/job-scraper.ts
@@ -0,0 +1,62 @@
+import puppeteer, { Browser } from "puppeteer";
+
+export async function fetchJobs(url: string): Promise<
+  {
+    title: string;
+    company: string;
+    closingDate: string;
+    postedDate: string;
+  }[]
+> {
+  let browser: Browser | null = null;
+
+  try {
+    browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setViewport({
+      width: 1080,
+      height: 768,
+    });
+
+    page.setDefaultNavigationTimeout(2 * 60 * 1000);
+
+    await page.goto(url);
+
+    const jobs: {
+      title: string;
+      company: string;
+      closingDate: string;
+      postedDate: string;
+    }[] = [];
+
+    const jobElements = await page.$$(
+      "div > div:nth-child(4) > div:nth-child(1) > div:nth-child(5)"
+    );
+
+    for (const el of jobElements) {
+      const title =
+        (await el.$eval("h4", (el) => el.textContent?.trim() || "")) ?? "";
+      const company =
+        (await el.$eval("h2", (el) => el.textContent?.trim() || "")) ?? "";
+      const closingDate =
+        (await el.$eval("p", (el) => el.textContent?.trim() || "")) ?? "";
+      const postedDate = "28-23-23";
+
+      jobs.push({
+        title,
+        company,
+        closingDate,
+        postedDate,
+      });
+    }
+
+    await browser.close();
+    return Promise.resolve(jobs);
+  } catch (error) {
+    if (browser) {
+      await browser.close();
+    }
+    return Promise.reject(error);
+  }
+}
